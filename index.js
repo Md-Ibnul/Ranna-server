cfa119d6ea5d03bd113c2926055bb8cc981af4de
Md-Ibnul <ibnulhasan456@gmail.com>
2023-05-03 19:33:42 +0600

    crashed fixed

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const chefs = require('./chefs.json');
-const chefDetails = require('./chefDetails.json');
-const recipes = require('./recipes.json');
+const chefs = require('./data/chefs.json');
+const chefDetails = require('./data/chefDetails.json');
+const recipes = require('./data/recipes.json');
 
 
 app.use(cors());
